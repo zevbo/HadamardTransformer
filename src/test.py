d3c35de832084aa96b230c7f04da6777fd7f6c63
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2024-12-06 17:49:49 -0500

    matching memory speed yay, but tensor cores are less acurate

--- src/test.py
+++ src/test.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
         test_hadamard_tensor_core(1)
         test_hadamard_tensor_core(128)
         test_hadamard_tensor_core(1024 * 16)
-        test_hadamard_tensor_core(1024 * 64)
+        test_hadamard_tensor_core(1024 * 128)
         test_hadamard_multi(size, 1)
         test_hadamard_multi(size, 2)
         test_hadamard_multi(size, 128)
